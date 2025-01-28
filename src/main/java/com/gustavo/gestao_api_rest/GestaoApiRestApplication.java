0e1ac80500416e428cc28f6608d3c7f6b9354eb0
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-01-27 23:25:12 -0300

    Projeto criado

---
+++ src/main/java/com/gustavo/gestao_api_rest/GestaoApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.gestao_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavo/gestao_api_rest/GestaoApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.gestao_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
