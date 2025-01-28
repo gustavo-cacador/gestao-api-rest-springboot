7c95e0e983ac0578db59b1d7e51946c4c3eaae4b
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-01-28 00:06:32 -0300

    entidade Funcionario

---
+++ src/main/java/com/gustavo/gestao_api_rest/entities/Funcionario.java
@@ -0,0 +1,116 @@
+package com.gustavo.gestao_api_rest.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_funcionario")
+public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_completo")
+    private String nome;
+
+    private String cpf;
+    private String endereco;
+    private Date dataNascimento;
+    private String emailPessoal;
+    private String emailWorkspace;
+    private String telefone;
+
+    public Funcionario() {
+
+    }
+
+    public Funcionario(Long id, String nome, String cpf, String endereco, Date dataNascimento, String emailPessoal, String emailWorkspace, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.dataNascimento = dataNascimento;
+        this.emailPessoal = emailPessoal;
+        this.emailWorkspace = emailWorkspace;
+        this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Funcionario that = (Funcionario) o;
+        return Objects.equals(id, that.id) && Objects.equals(cpf, that.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, cpf);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmailPessoal() {
+        return emailPessoal;
+    }
+
+    public void setEmailPessoal(String emailPessoal) {
+        this.emailPessoal = emailPessoal;
+    }
+
+    public String getEmailWorkspace() {
+        return emailWorkspace;
+    }
+
+    public void setEmailWorkspace(String emailWorkspace) {
+        this.emailWorkspace = emailWorkspace;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
