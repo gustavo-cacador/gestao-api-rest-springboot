bb16780f536f6a9065dc73a9db3395a7b4d3c7a0
Gustavo Caçador <gustavoirl456@outlook.com>
2025-01-28 15:52:46 -0300

    optional procurar por nome

--- src/main/java/com/gustavo/gestao_api_rest/repositories/FuncionarioRepository.java
+++ src/main/java/com/gustavo/gestao_api_rest/repositories/FuncionarioRepository.java
@@ -3,5 +3,8 @@ package com.gustavo.gestao_api_rest.repositories;
 import com.gustavo.gestao_api_rest.entities.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+    Optional<Funcionario> findByNomeLike(String nome);
 }
