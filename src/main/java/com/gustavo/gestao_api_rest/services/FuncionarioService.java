b4f137eea7507a84531583d5744c08aa6b12167d
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-01-29 01:26:55 -0300

    service funcionario

--- src/main/java/com/gustavo/gestao_api_rest/services/FuncionarioService.java
+++ src/main/java/com/gustavo/gestao_api_rest/services/FuncionarioService.java
@@ -4,9 +4,12 @@ import com.gustavo.gestao_api_rest.dto.FuncionarioDTO;
 import com.gustavo.gestao_api_rest.entities.Funcionario;
 import com.gustavo.gestao_api_rest.repositories.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -15,18 +18,58 @@ public class FuncionarioService {
     @Autowired
     public FuncionarioRepository funcionarioRepository;
 
-    public FuncionarioDTO buscarPorId(Long id) {
-        Funcionario result = funcionarioRepository.findById(id).get();
-        // Adicionar try para caso não exista o Id
-        return new FuncionarioDTO(result);
+    @Transactional(readOnly = true)
+    public FuncionarioDTO buscarPorId(Long id) throws ChangeSetPersister.NotFoundException {
+        try {
+            Funcionario result = funcionarioRepository.findById(id)
+                    .orElseThrow(() -> new NoSuchElementException("Funcionário com ID " + id + " não encontrado."));
+            return new FuncionarioDTO(result);
+        } catch (NoSuchElementException e) {
+            throw e;
+        }
     }
 
+    @Transactional(readOnly = true)
     public List<FuncionarioDTO> buscarTodos() {
         List<Funcionario> result = funcionarioRepository.findAll();
         return result.stream().map(x -> new FuncionarioDTO(x)).toList();
     }
 
-    public Optional<Funcionario> buscarPorNomeUnico(String nome) {
-        return funcionarioRepository.findByNomeLike(nome);
+    @Transactional
+    public FuncionarioDTO criarFuncionario(FuncionarioDTO dto) {
+        Funcionario entity = new Funcionario(dto);
+        entity = funcionarioRepository.save(entity);
+        return new FuncionarioDTO(entity);
     }
+
+    @Transactional
+    public FuncionarioDTO atualizarFuncionario(Long id, FuncionarioDTO dto) {
+        Funcionario funcionario = funcionarioRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Funcionário não encontrado."));
+        funcionario.setNome(dto.getNome());
+        funcionario.setEmailWorkspace(dto.getEmailWorkspace());
+        funcionario.setSetor(dto.getSetor());
+        funcionario = funcionarioRepository.save(funcionario);
+        return new FuncionarioDTO(funcionario);
+    }
+
+    @Transactional
+    public void deletarFuncionario(Long id) {
+        Optional<Funcionario> optional = funcionarioRepository.findById(id);
+        if(optional.isPresent()) {
+            funcionarioRepository.deleteById(id);
+        } else {
+            throw new NoSuchElementException("Esse funcionário não existe.");
+        }
+    }
+
+    /*
+    @Transactional
+    public void deletarFuncionario(Long id) {
+        if (funcionarioRepository.existsById(id)) {
+            funcionarioRepository.deleteById(id);
+        } else {
+            throw new NoSuchElementException("Funcionário não encontrado.");
+        }
+    } */
 }
