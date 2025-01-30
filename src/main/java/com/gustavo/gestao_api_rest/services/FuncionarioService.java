8bb655fd762e747d3d7dbb301eea2b270c289a1b
Gustavo Caçador <gustavoirl456@outlook.com>
2025-01-30 13:42:22 -0300

    funcionario service ok

--- src/main/java/com/gustavo/gestao_api_rest/services/FuncionarioService.java
+++ src/main/java/com/gustavo/gestao_api_rest/services/FuncionarioService.java
@@ -34,42 +34,4 @@ public class FuncionarioService {
         List<Funcionario> result = funcionarioRepository.findAll();
         return result.stream().map(x -> new FuncionarioDTO(x)).toList();
     }
-
-    @Transactional
-    public FuncionarioDTO criarFuncionario(FuncionarioDTO dto) {
-        Funcionario entity = new Funcionario(dto);
-        entity = funcionarioRepository.save(entity);
-        return new FuncionarioDTO(entity);
-    }
-
-    @Transactional
-    public FuncionarioDTO atualizarFuncionario(Long id, FuncionarioDTO dto) {
-        Funcionario funcionario = funcionarioRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("Funcionário não encontrado."));
-        funcionario.setNome(dto.getNome());
-        funcionario.setEmailWorkspace(dto.getEmailWorkspace());
-        funcionario.setSetor(dto.getSetor());
-        funcionario = funcionarioRepository.save(funcionario);
-        return new FuncionarioDTO(funcionario);
-    }
-
-    @Transactional
-    public void deletarFuncionario(Long id) {
-        Optional<Funcionario> optional = funcionarioRepository.findById(id);
-        if(optional.isPresent()) {
-            funcionarioRepository.deleteById(id);
-        } else {
-            throw new NoSuchElementException("Esse funcionário não existe.");
-        }
-    }
-
-    /*
-    @Transactional
-    public void deletarFuncionario(Long id) {
-        if (funcionarioRepository.existsById(id)) {
-            funcionarioRepository.deleteById(id);
-        } else {
-            throw new NoSuchElementException("Funcionário não encontrado.");
-        }
-    } */
 }
