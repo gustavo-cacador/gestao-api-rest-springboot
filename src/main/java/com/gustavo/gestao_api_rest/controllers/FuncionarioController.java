81e861703d132b771841ef76d986ec3df4ce9ccb
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-02-09 19:18:04 -0300

    atualizando controller

--- src/main/java/com/gustavo/gestao_api_rest/controllers/FuncionarioController.java
+++ src/main/java/com/gustavo/gestao_api_rest/controllers/FuncionarioController.java
@@ -1,15 +1,15 @@
 package com.gustavo.gestao_api_rest.controllers;
 
 import com.gustavo.gestao_api_rest.dto.FuncionarioDTO;
-import com.gustavo.gestao_api_rest.dto.FuncionarioMinDTO;
 import com.gustavo.gestao_api_rest.services.FuncionarioService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,14 +20,35 @@ public class FuncionarioController {
     private FuncionarioService funcionarioService;
 
     @GetMapping(value = "/{id}")
-    public FuncionarioDTO buscarPorId(@PathVariable Long id) throws ChangeSetPersister.NotFoundException {
-        FuncionarioDTO result = funcionarioService.buscarPorId(id);
-        return result;
+    public ResponseEntity<FuncionarioDTO> procurarPorId(@PathVariable Long id) {
+        FuncionarioDTO dto = funcionarioService.procurarPorId(id);
+        return ResponseEntity.ok(dto);
     }
 
     @GetMapping
-    public List<FuncionarioMinDTO> buscarTodos() {
-        List<FuncionarioMinDTO> result = funcionarioService.buscarTodos();
-        return result;
+    public List<FuncionarioDTO> buscarTodos() {
+        return funcionarioService.buscarTodos();
     }
+
+    @PostMapping
+    public ResponseEntity<FuncionarioDTO> inserirFuncionario(@Valid @RequestBody FuncionarioDTO dto) {
+        dto = funcionarioService.inserirFuncionario(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<FuncionarioDTO> atualizarFuncionario(@PathVariable Long id, @Valid @RequestBody FuncionarioDTO dto) {
+        dto = funcionarioService.atualizarFuncionario(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deletarFuncionario(@PathVariable Long id) {
+        funcionarioService.deletarFuncionario(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
